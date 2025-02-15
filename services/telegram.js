199814a39ff080f49ee4536f059d9483c62bcf39
Isacc Gosme Palacios <143350307+Isaccr7@users.noreply.github.com>
2025-02-15 12:45:17 -0500

    12

--- services/telegram.js
+++ services/telegram.js
@@ -1,7 +1,7 @@
 import { telegram_api, telegram_token } from "../utils/config";
 
 export async function sendToTelegram(chatId,messages) {
-    return fetch(`${telegram_token}/sendMessage`,{
+    return fetch(`${telegram_api}/sendMessage`,{
         method:"POST",
         headers:{"Content-type":"application/json"},
         body:JSON.stringify({chat_id:chatId,text:messages}),
