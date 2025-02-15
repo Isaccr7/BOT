470438e16cc2874af6af6bc2ea18f34519a19c07
Isacc Gosme Palacios <143350307+Isaccr7@users.noreply.github.com>
2025-02-15 13:20:47 -0500

    ee

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 import { sendToTelegram } from "./services/telegram";
 
-export default {
-    async fetch(request) {
-        if (request.method !== "POST") {
-            return new Response("Este metodo no funciona", { status: 405 })
+export default{
+    async fetch(request){
+        if(request.method!=="POST"){
+            return new Response("Este metodo no funciona",{status:405})
         }
-        /*creamos un try catch*/
-        try {
-            const data = await request.json();
-            let platform, message, chatId;
-            /*verificando de donde viene la peticion*/
-            /* if (data.message) {
-                platform = "telegram";
-                chatId = data.message.chat.id;
-                message = data.message.text || "";
-                 
-            }
-            else { */
-                platform = data.platform;
-                chatId = data.chatId;
-                message = data.message;
+
+
+        /*creamo un try cath */
+        try{
+            const data=await request.json();
+            let platform,message,chatId;
+
+            // verifica,ps de donde viene la peticion
+
+         /*   if(data.messages){
+                platform="telegram";
+                chatId=data.message.chat.id;
+                messages=data.message.text||"";
+            } */
+
+            // si es de la api
+       /*     else{ */
+                platform=data.platform;
+                chatId=data.chatId;
+                message=data.message;
+
             /* } */
 
-            /*verificar a que plataforma corresponde*/
+            // verificamos a que platafoma corresponde 
 
-            if (platform === "telegram") {
-                await sendToTelegram(chatId, message);
+            if(platform==="telegram"){
+                await sendToTelegram(chatId,message);
             }
-            else {
-                return new Response("Esta plataforma no esta disponible", { status: 400 });
+            else{
+                return new Response("Esta plataforma no esta disponible",{status:400});
             }
             /*responder*/
-            return new Response(`Mensaje Enviado ${message}`, { status: 200 });
 
-        } catch (error) {
-            return new Response(`algo sucedio ${error.message}`, { status: 500 });
+            return new Response(`Mensaje Enviado ${message}`,{status:200});
+
+        }catch(error){
+            return new Response(`Algo sucedio ${error.message}`,{status:500});
         }
     }
 };
-/* {
-    name="ruiz",
-    edad="25",
-    dir="no se sabe"
-} */
