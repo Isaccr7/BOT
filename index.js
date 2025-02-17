0b9f54201667aa4c2bc636e3c0da2f1d7d68b2e0
Isacc Gosme Palacios <143350307+Isaccr7@users.noreply.github.com>
2025-02-17 11:12:26 -0500

    xdx

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-import { sendToTelegram } from "./services/telegram";
+    import { sendToTelegram } from "./services/telegram";
 
-export default{
-    async fetch(request){
-        if(request.method!=="POST"){
-            return new Response("Este metodo no funciona",{status:405})
-        }
-
-
-        /*creamo un try cath */
-        try{
-            const data=await request.json();
-            let platform,message,chatId;
-
-            // verifica,ps de donde viene la peticion
-
-         /*   if(data.messages){
-                platform="telegram";
-                chatId=data.message.chat.id;
-                messages=data.message.text||"";
-            } */
-
-            // si es de la api
-       /*     else{ */
-                platform=data.platform;
-                chatId=data.chatId;
-                message=data.message;
-
-            /* } */
-
-            // verificamos a que platafoma corresponde 
-
-            if(platform==="telegram"){
-                await sendToTelegram(chatId,message);
+    export default{
+        async fetch(request){
+            if(request.method!=="POST"){
+                return new Response("Este metodo no funciona",{status:405})
             }
-            else{
-                return new Response("Esta plataforma no esta disponible",{status:400});
+
+
+            /*creamo un try cath */
+            try{
+                const data=await request.json();
+                let platform,message,chatId;
+
+                // verifica,ps de donde viene la peticion
+
+            /*   if(data.messages){
+                    platform="telegram";
+                    chatId=data.message.chat.id;
+                    messages=data.message.text||"";
+                } */
+
+                // si es de la api
+        /*     else{ */
+                    platform=data.platform;
+                    chatId=data.chatId;
+                    message=data.message;
+
+                /* } */
+
+                // verificamos a que platafoma corresponde 
+
+                if(platform==="telegram"){
+                    await sendToTelegram(chatId,message);
+                }
+                else{
+                    return new Response("Esta plataforma no esta disponible",{status:400});
+                }
+                /*responder*/
+
+                return new Response(`Mensaje Enviado ${message}`,{status:200});
+
+            }catch(error){
+                return new Response(`Algo sucedio ${error.message}`,{status:500});
             }
-            /*responder*/
-
-            return new Response(`Mensaje Enviado ${message}`,{status:200});
-
-        }catch(error){
-            return new Response(`Algo sucedio ${error.message}`,{status:500});
         }
-    }
-};
+    };
